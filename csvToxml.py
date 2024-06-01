9fc278f24b760a3cd01dafa7b8f2f199a95bdfd5
thrashhhh1 <benjatejoh@gmail.com>
2024-06-01 14:21:45 -0400

    update csvToxml

--- csvToxml.py
+++ csvToxml.py
@@ -2,18 +2,11 @@ import pandas as pd
 
 data = pd.read_csv('partidosCampeonatoChileno.csv', header=0)
 
-data_ = []
+dataFiltred = []
+dataArray = data.values.tolist()
 
-# Iterar sobre las filas del DataFrame
-for index, row in data.iterrows():
-    # Verificar si la columna 'Local' es igual a 'Concón National'
-    if row['Local'] == 'Concón National':
-        data_.append(row)
-
-    if row['Visita'] == 'Concón National':
-        data_.append(row)    
-
-# Convertir data_ a un DataFrame si es necesario
-filtered_data = pd.DataFrame(data_)
-
-print(filtered_data)
+for array in dataArray:
+    if array[1] == 'Lautaro de Buin':
+        dataFiltred.append(array)
+    
+print(dataFiltred)
